7ff6511cf243d7a221c555a162cfd107c907004d
Daniel-dzp <15030090@itcelaya.edu.mx>
2020-03-04 18:38:49 -0600

    Se corrigio algunos errores.

--- src/pruebas/VentanaPruebasController.java
+++ src/pruebas/VentanaPruebasController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pruebas;
 
 import java.net.URL;
@@ -10,18 +5,11 @@ import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
 /**
- * FXML Controller class
- *
  * @author dzp
  */
 public class VentanaPruebasController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
 }
